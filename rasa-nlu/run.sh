de4552ed14459a8dd26b0784695f9203eb4a6693
chedim <chedim@chedim.com>
2023-04-27 18:38:28 -0400

    rasa-nlu initial files

---
+++ rasa-nlu/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : chedim (chedim@couchbaser)
+# @file        : run
+# @created     : Thursday Apr 27, 2023 18:37:42 EDT
+#
+# @description : rasa-nlu launch script
+######################################################################
+
+
+echo "hello, rasa"
+
